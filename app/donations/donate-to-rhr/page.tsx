eb8166f8835e6b8eb4caa37b0eb3d0dacc363aae
Hamzah Alani <80855152+Alani25@users.noreply.github.com>
2025-10-27 14:28:11 -0400

    Update page.tsx

--- app/donations/donate-to-rhr/page.tsx
+++ app/donations/donate-to-rhr/page.tsx
@@ -1,3 +1,10 @@
 export default function DonatePage() {
-  return <div>Donation page setup error</div>
+  return <div><h1>Donation page stub</h1> <p>This donation page hasn't been set up to accept online donations yet! If you wish to donate please directly contact RHR at:
+    <ul>
+      <li><b>Phone#:</b> (585) 563-7747</li>
+      <li><b>Email:</b> rhr@rhrroc.org</li>
+      <li><b>Location:</b> 228 South Plymouth, Rochester, NY 14608</li>
+      </ul>
+    
+    </p></div>
 }
